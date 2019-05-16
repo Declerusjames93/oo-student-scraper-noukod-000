0c4542a4cfb8851f21aa21dd8d93deb364fc290c
James Declerus <james.declerus@esih.edu>
2019-05-16 14:58:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,9 +25,9 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
 
-    doc = Nokogiri::HTML(open(profile_url))
+    html = Nokogiri::HTML(open(profile_url))
     student = {}
-    link = doc.css(".social-icon-container a").collect do |icon|
+    link = html.css(".social-icon-container a").collect do |icon|
     icon.attribute("href").value
     end
 
